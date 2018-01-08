30a8fe1a7c67180be60d7bb01377cdb563ebeaea
David Stout <davidgstout63@gmail.com>
2018-01-08 17:37:50 -0500

    Added delete functionality to li

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -3,3 +3,11 @@ $("li").click(function(){
 	$(this).toggleClass("completed");
 
 	});
+
+$("span").click(function(event){
+	$(this).parent().fadeOut(2000,function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
